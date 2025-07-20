3f73e1716cc12b2103c60c9e2abed690760177eb
fragment veiled <cody2j2reed@gmail.com>
2025-07-20 18:31:03 -0400

    complete 'material' THREE lesson

--- exampleFive/src/script.js
+++ exampleFive/src/script.js
@@ -8,6 +8,30 @@ import {
 //Scene
 const scene = new THREE.Scene();
 
+// Lights
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+const pointLight = new THREE.PointLight(0xffffff, 0.5)
+pointLight.position.set(2, 2, 2)
+scene.add(ambientLight, pointLight)
+
+const textureLoader = new THREE.TextureLoader()
+const colorTexture = textureLoader.load('/texture/color.jpg')
+const matcapTexture = textureLoader.load('/texture/mat2.png')
+const bumpTexture = textureLoader.load('/texture/bump.jpg')
+const displacementTexture = textureLoader.load('/texture/displacementMap.jpg')
+
+// cubeTextureLoader
+const cubeTextureLoader = new THREE.CubeTextureLoader()
+const envTexture = cubeTextureLoader.load([
+  "/texture/env/px.png",
+  "/texture/env/nx.png",
+  "/texture/env/py.png",
+  "/texture/env/ny.png",
+  "/texture/env/pz.png",
+  "/texture/env/nz.png",
+])
+// scene.background = envTexture
+
 //Resizing
 window.addEventListener("resize", () => {
   //Update Size
@@ -24,8 +48,25 @@ window.addEventListener("resize", () => {
 });
 
 //Mesh
-const geometry = new THREE.PlaneBufferGeometry(1, 1);
-const material = new THREE.MeshBasicMaterial({ color: "purple" });
+const geometry = new THREE.SphereGeometry( 0.5, 32, 32);
+const material = new THREE.MeshStandardMaterial();
+// material.map = colorTexture
+// material.wireframe = true
+// material.color = new THREE.Color('skyblue')
+// material.transparent = true
+// material.opacity = 0.4
+// material.side = THREE.DoubleSide
+// material.visible = false
+// material.matcap = matcapTexture
+// material.shininess = 200
+// material.specular = new THREE.Color("green")
+material.metalness = 0.9
+material.roughness = 0.1
+// material.bumpMap = bumpTexture
+// material.displacementMap = displacementTexture
+material.envMap = envTexture
+
+
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
